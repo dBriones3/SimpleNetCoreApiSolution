4eb76b7d28b5be60276dadad39967b234667e8a9
Daniel Briones <daniel.briones.perez@gmail.com>
2020-10-24 15:47:52 -0500

    Add unit test todoListController

--- SimpleNetCoreApiTests/TodoItemsControllerTests.cs
+++ SimpleNetCoreApiTests/TodoItemsControllerTests.cs
@@ -6,6 +6,7 @@ using SimpleNetCoreApi.Models;
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xunit;
@@ -14,16 +15,15 @@ namespace SimpleNetCoreApiTests
 {
     public class TodoItemsControllerTests
     {
+
         [Fact]
         public async Task ShouldAddAnItem()
         {
-
             var _options = new DbContextOptionsBuilder<TodoContext>()
-            .UseInMemoryDatabase("TodoListDatabase")
+            .UseInMemoryDatabase(new Guid().ToString())
             .Options;
 
             var todoContex = new TodoContext(_options);
-
             var todoItemsEndpoint = new TodoItemsController(todoContex);
             var item = new TodoItem
             {
@@ -33,9 +33,97 @@ namespace SimpleNetCoreApiTests
 
             var response = await todoItemsEndpoint.PostTodoItem(item);
 
-            var result = (OkObjectResult)response.Result;
+            
+            Assert.Equal(item, ((ObjectResult)response.Result).Value);
+        }
 
-            Assert.True(result.Value != null);
+        [Fact]
+        public async Task ShouldGetItems()
+        {
+            var _options = new DbContextOptionsBuilder<TodoContext>()
+            .UseInMemoryDatabase(new Guid().ToString())
+            .Options;
+
+            var todoContex = new TodoContext(_options);
+            var todoItemsEndpoint = new TodoItemsController(todoContex);
+            var item1 = new TodoItem
+            {
+                Name = "Clean my room",
+                IsComplete = false
+            };
+
+            var item2 = new TodoItem
+            {
+                Name = "Make unit test",
+                IsComplete = false
+            };
+
+            await todoItemsEndpoint.PostTodoItem(item1);
+            await todoItemsEndpoint.PostTodoItem(item2);
+
+            var getResponse = await todoItemsEndpoint.GetTodoItems();
+
+            Assert.True(getResponse.Value.Count() == 2);
+        }
+
+        [Fact]
+        public async Task ShouldGetAnItem()
+        {
+            var _options = new DbContextOptionsBuilder<TodoContext>()
+            .UseInMemoryDatabase(new Guid().ToString())
+            .Options;
+
+            var todoContex = new TodoContext(_options);
+            var todoItemsEndpoint = new TodoItemsController(todoContex);
+            var item1 = new TodoItem
+            {
+                Name = "Clean my room",
+                IsComplete = false
+            };
+
+            var item2 = new TodoItem
+            {
+                Name = "Make unit test",
+                IsComplete = false
+            };
+
+            await todoItemsEndpoint.PostTodoItem(item1);
+            await todoItemsEndpoint.PostTodoItem(item2);
+
+            var getResponse = await todoItemsEndpoint.GetTodoItem(1);
+
+            Assert.True(getResponse.Value.Id == 1);
+        }
+
+        [Fact]
+        public async Task ShouldDeleteAnItem()
+        {
+            var _options = new DbContextOptionsBuilder<TodoContext>()
+            .UseInMemoryDatabase(new Guid().ToString())
+            .Options;
+
+            var todoContex = new TodoContext(_options);
+            var todoItemsEndpoint = new TodoItemsController(todoContex);
+            var item1 = new TodoItem
+            {
+                Name = "Clean my room",
+                IsComplete = false
+            };
+
+            var item2 = new TodoItem
+            {
+                Name = "Make unit test",
+                IsComplete = false
+            };
+
+            await todoItemsEndpoint.PostTodoItem(item1);
+            await todoItemsEndpoint.PostTodoItem(item2);
+
+            var response = await todoItemsEndpoint.DeleteTodoItem(1);
+
+            var getResponse = await todoItemsEndpoint.GetTodoItems();
+
+            Assert.True(response.Value.Id == 1);
         }
     }
 }
